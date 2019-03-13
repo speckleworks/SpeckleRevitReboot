dc23922cc3572e9e097e898555e97b2121599d41
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-03-13 11:52:33 +0000

    adds client serialisation logic

--- SpeckleRevitReboot/UI/SpeckleUiBindings.cs
+++ SpeckleRevitReboot/UI/SpeckleUiBindings.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Newtonsoft.Json;
+using SpeckleRevitReboot.ClientStorage;
 using SpeckleUiBase;
 
 namespace SpeckleRevitReboot.UI
@@ -14,22 +17,48 @@ namespace SpeckleRevitReboot.UI
   {
 
     public UIApplication RevitApp;
+    public UIDocument CurrentDoc { get => RevitApp.ActiveUIDocument; }
     public List<Action> Queue;
     public ExternalEvent Executor;
+
+    public SpeckleClientsWrapper myClientList;
 
     public SpeckleUiBindingsRevit( UIApplication _RevitApp ) : base()
     {
       RevitApp = _RevitApp;
       Queue = new List<Action>();
-
+      myClientList = new SpeckleClientsWrapper();
       // TODO: 
       // Scan the file for any existing clients and populate the MyClients list
       // Set event handlers for Document loading and unloading
     }
 
     #region clients
-    public override void AddSender( string args ) { }
-    public override void AddReceiver( string args ) { }
+    public override void AddSender( string args )
+    {
+      //TODO
+    }
+
+    public override void AddReceiver( string args )
+    {
+      //TODO
+      var client = JsonConvert.DeserializeObject<dynamic>( args );
+      myClientList.clients.Add( client );
+
+      Queue.Add( new Action( ( ) =>
+      {
+
+        using ( Transaction t = new Transaction( CurrentDoc.Document, "adding receiver" ) )
+        {
+          t.Start();
+          SpeckleClientsStorage.WriteClients( CurrentDoc.Document, myClientList );
+          t.Commit();
+        }
+
+      } ) );
+      Executor.Raise();
+    }
+
     public override void RemoveClient( string args ) { }
     #endregion
 
@@ -37,11 +66,10 @@ namespace SpeckleRevitReboot.UI
     {
       Queue.Add( new Action( ( ) =>
       {
-        Debug.WriteLine( "Should bake: " + args );
+        Debug.WriteLine( "Should bake client: " + args );
       } ) );
 
       Executor.Raise();
-      //throw new NotImplementedException();
     }
 
     public override void AddObjectsToSender( string args )
@@ -56,7 +84,10 @@ namespace SpeckleRevitReboot.UI
 
     public override string GetFileClients( )
     {
-      throw new NotImplementedException();
+      var myReadClients = SpeckleClientsStorage.ReadClients( CurrentDoc.Document );
+      if ( myReadClients == null )
+        myReadClients = new SpeckleClientsWrapper();
+      return JsonConvert.SerializeObject( myReadClients.clients );
     }
 
     public override string GetApplicationHostName( )
@@ -66,17 +97,18 @@ namespace SpeckleRevitReboot.UI
 
     public override string GetFileName( )
     {
-      return "Somewhere in Revit. Not implemented :)";
+      return CurrentDoc.Document.Title;
     }
 
     public override string GetDocumentId( )
     {
-      return "testing";
+      // TODO: return proper id?
+      return CurrentDoc.Document.PathName + CurrentDoc.Document.Title;
     }
 
     public override string GetDocumentLocation( )
     {
-      return "where youve put it";
+      return CurrentDoc.Document.PathName;
     }
   }
 }
