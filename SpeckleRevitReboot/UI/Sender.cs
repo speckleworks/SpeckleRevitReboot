2e21eabb204ae48dcf9f6c10642bf72d76b1e196
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-05-01 16:43:36 +0100

    smaller pagination and try catches around the sensitive parts of the sending process

--- SpeckleRevitReboot/UI/Sender.cs
+++ SpeckleRevitReboot/UI/Sender.cs
@@ -39,15 +39,21 @@ namespace SpeckleRevit.UI
           loadingBlurb = string.Format( "Converting objects: {0} / {1}", i, client.objects.Count )
         } ) );
 
-
-        var revitElement = CurrentDoc.Document.GetElement( ( string ) obj.id );
-        var conversionResult = SpeckleCore.Converter.Serialise( revitElement );
-        convertedObjects.Add( conversionResult );
+        try
+        {
+          var revitElement = CurrentDoc.Document.GetElement( ( string ) obj.id );
+          var conversionResult = SpeckleCore.Converter.Serialise( revitElement );
+          convertedObjects.Add( conversionResult );
+        }
+        catch ( Exception e )
+        {
+          // TODO: Bubble it up
+        }
       }
 
       LocalContext.PruneExistingObjects( convertedObjects, apiClient.BaseUrl );
 
-      var chunks = convertedObjects.ChunkBy( 84 );
+      var chunks = convertedObjects.ChunkBy( 5 );
       var placeholders = new List<SpeckleObject>();
 
       i = 0;
@@ -62,29 +68,36 @@ namespace SpeckleRevit.UI
           loadingBlurb = string.Format( "Uploading {0} / {1}", i, client.objects.Count )
         } ) );
 
-        var chunkResponse = apiClient.ObjectCreateAsync( chunk ).Result.Resources;
-
-        int m = 0;
-        foreach ( var obj in chunk )
+        try
         {
-          obj._id = chunkResponse[ m++ ]._id;
-          placeholders.Add( new SpecklePlaceholder() { _id = obj._id } );
-        }
+          var chunkResponse = apiClient.ObjectCreateAsync( chunk ).Result.Resources;
 
-        Task.Run( ( ) =>
-        {
+          int m = 0;
           foreach ( var obj in chunk )
           {
-            if ( obj.Type != "Placeholder" ) LocalContext.AddSentObject( obj, apiClient.BaseUrl );
+            obj._id = chunkResponse[ m++ ]._id;
+            placeholders.Add( new SpecklePlaceholder() { _id = obj._id } );
           }
-        } );
+
+          Task.Run( ( ) =>
+          {
+            foreach ( var obj in chunk )
+            {
+              if ( obj.Type != "Placeholder" ) LocalContext.AddSentObject( obj, apiClient.BaseUrl );
+            }
+          } );
+        }
+        catch ( Exception e )
+        {
+          //TODO: Bubble it up...
+        }
       }
 
       var myStream = new SpeckleStream() { Objects = placeholders };
 
       var ug = UnitUtils.GetUnitGroup( UnitType.UT_Length );
       var baseProps = new Dictionary<string, object>();
-      
+
       // TODO: format units to something rational
       baseProps[ "units" ] = CurrentDoc.Document.GetUnits().ToString();
       baseProps[ "units_secondtry" ] = ug.ToString();
