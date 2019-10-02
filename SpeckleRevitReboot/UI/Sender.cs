ed6d96b52eda45e22e35d1eb62063732568ab96a
matteo@cominetti.org <matteo.cominetti@arup.com>
2019-10-02 16:09:29 +0100

    fixed filter by prop with strings

--- SpeckleRevitReboot/UI/Sender.cs
+++ SpeckleRevitReboot/UI/Sender.cs
@@ -49,16 +49,16 @@ namespace SpeckleRevit.UI
       ISelectionFilter filter = JsonConvert.DeserializeObject(JsonConvert.SerializeObject(client.filter), GetFilterType(client.filter.Type.ToString()));
       GetSelectionFilterObjects(filter, client._id.ToString(), client.streamId.ToString());
 
-      SpeckleTelemetry.RecordStreamCreated( "Revit" );
+      SpeckleTelemetry.RecordStreamCreated("Revit");
     }
 
     public override void UpdateSender(string args)
     {
       var client = JsonConvert.DeserializeObject<dynamic>(args);
-      var index = ClientListWrapper.clients.FindIndex( cl => (string) cl._id == (string) client._id );
+      var index = ClientListWrapper.clients.FindIndex(cl => (string)cl._id == (string)client._id);
       ClientListWrapper.clients[index] = client;
 
-      var myStream = LocalState.FirstOrDefault(st => st.StreamId == (string) client.streamId);
+      var myStream = LocalState.FirstOrDefault(st => st.StreamId == (string)client.streamId);
       myStream.Name = (string)client.name;
 
       Queue.Add(new Action(() =>
@@ -76,7 +76,7 @@ namespace SpeckleRevit.UI
       ISelectionFilter filter = JsonConvert.DeserializeObject(JsonConvert.SerializeObject(client.filter), GetFilterType(client.filter.Type.ToString()));
       GetSelectionFilterObjects(filter, client._id.ToString(), client.streamId.ToString());
 
-      SpeckleTelemetry.RecordStreamUpdated( "Revit" );
+      SpeckleTelemetry.RecordStreamUpdated("Revit");
     }
 
     // NOTE: This is actually triggered when clicking "Push!"
@@ -84,9 +84,9 @@ namespace SpeckleRevit.UI
     // Create buckets, send sequentially, notify ui re upload progress
     // NOTE: Problems with local context and cache: we seem to not sucesffuly pass through it
     // perhaps we're not storing the right sent object (localcontext.addsentobject)
-    public override void PushSender( string args )
+    public override void PushSender(string args)
     {
-      var client =  JsonConvert.DeserializeObject<dynamic>( args );
+      var client = JsonConvert.DeserializeObject<dynamic>(args);
 
       //if it's a category or property filter we need to refresh the list of objects
       //if it's a selection filter just use the objects that were stored previously
@@ -95,130 +95,130 @@ namespace SpeckleRevit.UI
 
       objects = GetSelectionFilterObjects(filter, client._id.ToString(), client.streamId.ToString());
 
-      var apiClient = new SpeckleApiClient( (string) client.account.RestApi ) { AuthToken = (string) client.account.Token };
+      var apiClient = new SpeckleApiClient((string)client.account.RestApi) { AuthToken = (string)client.account.Token };
 
       var convertedObjects = new List<SpeckleObject>();
       var placeholders = new List<SpeckleObject>();
 
-      var units = CurrentDoc.Document.GetUnits().GetFormatOptions( UnitType.UT_Length ).DisplayUnits.ToString().ToLower().Replace( "dut_", "" );
-      InjectScaleInKits( GetScale( units ) ); // this is used for feet to sane units conversion.
+      var units = CurrentDoc.Document.GetUnits().GetFormatOptions(UnitType.UT_Length).DisplayUnits.ToString().ToLower().Replace("dut_", "");
+      InjectScaleInKits(GetScale(units)); // this is used for feet to sane units conversion.
 
       int i = 0;
       long currentBucketSize = 0;
       var errors = "";
       var failedSend = 0;
       var failedConvert = 0;
-      foreach( var obj in objects)
+      foreach (var obj in objects)
       {
-        NotifyUi( "update-client",  JsonConvert.SerializeObject( new
+        NotifyUi("update-client", JsonConvert.SerializeObject(new
         {
-          _id = (string) client._id,
+          _id = (string)client._id,
           loading = true,
           isLoadingIndeterminate = false,
           loadingProgress = 1f * i++ / objects.Count() * 100,
-          loadingBlurb = string.Format( "Converting and uploading objects: {0} / {1}", i, objects.Count() )
-        } ) );
+          loadingBlurb = string.Format("Converting and uploading objects: {0} / {1}", i, objects.Count())
+        }));
 
         try
         {
-          var revitElement = CurrentDoc.Document.GetElement( (string) obj.Properties[ "revitUniqueId" ] );
+          var revitElement = CurrentDoc.Document.GetElement((string)obj.Properties["revitUniqueId"]);
 
-          var conversionResult = SpeckleCore.Converter.Serialise( new List<object>() { revitElement } );
-          var byteCount = Converter.getBytes( conversionResult ).Length;
+          var conversionResult = SpeckleCore.Converter.Serialise(new List<object>() { revitElement });
+          var byteCount = Converter.getBytes(conversionResult).Length;
           currentBucketSize += byteCount;
 
-          if( byteCount > 2e6 )
+          if (byteCount > 2e6)
           {
             var problemId = revitElement.Id;
           }
 
-          convertedObjects.AddRange( conversionResult );
+          convertedObjects.AddRange(conversionResult);
 
-          if( currentBucketSize > 5e5 || i >= objects.Count() ) // aim for roughly 500kb uncompressed
+          if (currentBucketSize > 5e5 || i >= objects.Count()) // aim for roughly 500kb uncompressed
           {
-            LocalContext.PruneExistingObjects( convertedObjects, apiClient.BaseUrl );
+            LocalContext.PruneExistingObjects(convertedObjects, apiClient.BaseUrl);
 
             try
             {
-              var chunkResponse = apiClient.ObjectCreateAsync( convertedObjects ).Result.Resources;
+              var chunkResponse = apiClient.ObjectCreateAsync(convertedObjects).Result.Resources;
               int m = 0;
-              foreach( var objConverted in convertedObjects )
+              foreach (var objConverted in convertedObjects)
               {
-                objConverted._id = chunkResponse[ m++ ]._id;
-                placeholders.Add( new SpecklePlaceholder() { _id = objConverted._id } );
-                if( objConverted.Type != "Placeholder" ) LocalContext.AddSentObject( objConverted, apiClient.BaseUrl );
+                objConverted._id = chunkResponse[m++]._id;
+                placeholders.Add(new SpecklePlaceholder() { _id = objConverted._id });
+                if (objConverted.Type != "Placeholder") LocalContext.AddSentObject(objConverted, apiClient.BaseUrl);
               }
             }
-            catch( Exception e )
+            catch (Exception e)
             {
               failedSend += convertedObjects.Count;
-              NotifyUi( "update-client",  JsonConvert.SerializeObject( new
+              NotifyUi("update-client", JsonConvert.SerializeObject(new
               {
-                _id = (string) client._id,
+                _id = (string)client._id,
                 errors = "Failed to send " + failedSend + " objects."
-              } ) );
+              }));
             }
             currentBucketSize = 0;
             convertedObjects = new List<SpeckleObject>(); // reset the chunkness
           }
         }
-        catch( Exception e )
+        catch (Exception e)
         {
           failedConvert++;
-          NotifyUi( "update-client",  JsonConvert.SerializeObject( new
+          NotifyUi("update-client", JsonConvert.SerializeObject(new
           {
-            _id = (string) client._id,
+            _id = (string)client._id,
             errors = "Failed to convert " + failedConvert + " objects."
-          } ) );
+          }));
         }
       }
 
-      if( failedConvert > 0 ) errors += String.Format( "Failed to convert a total of {0} objects. ", failedConvert );
-      if( failedSend > 0 ) errors += String.Format( "Failed to send a total of {0} objects. ", failedSend );
+      if (failedConvert > 0) errors += String.Format("Failed to convert a total of {0} objects. ", failedConvert);
+      if (failedSend > 0) errors += String.Format("Failed to send a total of {0} objects. ", failedSend);
 
       var myStream = new SpeckleStream() { Objects = placeholders };
 
-      var ug = UnitUtils.GetUnitGroup( UnitType.UT_Length );
+      var ug = UnitUtils.GetUnitGroup(UnitType.UT_Length);
       var baseProps = new Dictionary<string, object>();
 
-      baseProps[ "units" ] = units;
+      baseProps["units"] = units;
 
-      baseProps[ "unitsDictionary" ] = GetAndClearUnitDictionary();
+      baseProps["unitsDictionary"] = GetAndClearUnitDictionary();
 
       myStream.BaseProperties = baseProps;
       //myStream.BaseProperties =  JsonConvert.SerializeObject(baseProps);
 
-      NotifyUi( "update-client",  JsonConvert.SerializeObject( new
+      NotifyUi("update-client", JsonConvert.SerializeObject(new
       {
-        _id = (string) client._id,
+        _id = (string)client._id,
         loading = true,
         isLoadingIndeterminate = true,
         loadingBlurb = "Updating stream."
-      } ) );
+      }));
 
-      var response = apiClient.StreamUpdateAsync( (string) client.streamId, myStream ).Result;
+      var response = apiClient.StreamUpdateAsync((string)client.streamId, myStream).Result;
 
       var plural = objects.Count() == 1 ? "" : "s";
-      NotifyUi( "update-client",  JsonConvert.SerializeObject( new
+      NotifyUi("update-client", JsonConvert.SerializeObject(new
       {
-        _id = (string) client._id,
+        _id = (string)client._id,
         loading = false,
         loadingBlurb = "",
         message = $"Done sending {objects.Count()} object{plural}.",
         errors
-      } ) );
+      }));
 
-      SpeckleTelemetry.RecordStreamUpdated( "Revit" );
+      SpeckleTelemetry.RecordStreamUpdated("Revit");
     }
 
     /// <summary>
     /// Pass selected element ids to UI
     /// </summary>
     /// <param name="args"></param>
-    public override void AddSelectionToSender( string args )
+    public override void AddSelectionToSender(string args)
     {
       var doc = CurrentDoc.Document;
-      var selectedObjects = CurrentDoc != null ?  CurrentDoc.Selection.GetElementIds().Select(id => doc.GetElement(id).UniqueId).ToList() : new List<string>();
+      var selectedObjects = CurrentDoc != null ? CurrentDoc.Selection.GetElementIds().Select(id => doc.GetElement(id).UniqueId).ToList() : new List<string>();
 
       NotifyUi("update-selection", JsonConvert.SerializeObject(new
       {
@@ -226,7 +226,7 @@ namespace SpeckleRevit.UI
       }));
     }
 
-    public override void RemoveSelectionFromSender( string args )
+    public override void RemoveSelectionFromSender(string args)
     {
       // NOT IMPLEMENTED
 
@@ -310,7 +310,7 @@ namespace SpeckleRevit.UI
         selectionIds = new FilteredElementCollector(doc)
           .WhereElementIsNotElementType()
           .WherePasses(categoryFilter)
-          .Select(x=>x.UniqueId).ToList();
+          .Select(x => x.UniqueId).ToList();
 
       }
       else if (filter.Name == "Parameter")
@@ -320,22 +320,23 @@ namespace SpeckleRevit.UI
           var propFilter = filter as PropertySelectionFilter;
           var query = new FilteredElementCollector(doc)
             .WhereElementIsNotElementType()
+            .WhereElementIsViewIndependent()
+            .OfClass(typeof(FamilyInstance))
             .Where(fi => fi.LookupParameter(propFilter.PropertyName) != null);
 
-          
-
+          propFilter.PropertyValue = propFilter.PropertyValue.ToLowerInvariant();
 
           switch (propFilter.PropertyOperator)
           {
             case "equals":
-              query = query.Where(fi => fi.LookupParameter(propFilter.PropertyName).AsValueString() == propFilter.PropertyValue);
+              query = query.Where(fi => GetStringValue(fi.LookupParameter(propFilter.PropertyName)) == propFilter.PropertyValue);
               break;
             case "contains":
-              query = query.Where(fi => fi.LookupParameter(propFilter.PropertyName).AsValueString().Contains(propFilter.PropertyValue));
+              query = query.Where(fi => GetStringValue(fi.LookupParameter(propFilter.PropertyName)).Contains(propFilter.PropertyValue));
               break;
             case "is greater than":
               query = query.Where(fi => UnitUtils.ConvertFromInternalUnits(
-                fi.LookupParameter(propFilter.PropertyName).AsDouble(), 
+                fi.LookupParameter(propFilter.PropertyName).AsDouble(),
                 fi.LookupParameter(propFilter.PropertyName).DisplayUnitType) > double.Parse(propFilter.PropertyValue));
               break;
             case "is less than":
@@ -350,7 +351,10 @@ namespace SpeckleRevit.UI
           selectionIds = query.Select(x => x.UniqueId).ToList();
 
         }
-        catch { }
+        catch (Exception e)
+        {
+          Console.WriteLine(e);
+        }
       }
 
       // LOCAL STATE management
@@ -394,6 +398,19 @@ namespace SpeckleRevit.UI
         }));
 
       return objects;
+    }
+
+    private string GetStringValue(Parameter p)
+    {
+      string value = "";
+      if (!p.HasValue)
+        return value;
+      if (string.IsNullOrEmpty(p.AsValueString()) && string.IsNullOrEmpty(p.AsString()))
+        return value;
+      if (!string.IsNullOrEmpty(p.AsValueString()))
+        return p.AsValueString().ToLowerInvariant();
+      else
+        return p.AsString().ToLowerInvariant();
     }
     #endregion
 
